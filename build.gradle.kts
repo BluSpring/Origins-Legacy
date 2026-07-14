6ee484822e5673913f4898d361401741adba68f2
BluSpring <bluspringindohq@gmail.com>
2026-07-14 20:18:05 +0800

    this took so long to fix publishing, my god

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ import java.net.URI
 plugins {
 	id("fabric-loom") version "1.10-SNAPSHOT"
 	`maven-publish`
-	id("me.modmuss50.mod-publish-plugin") version "0.7.+"
+	id("me.modmuss50.mod-publish-plugin") version "2.1.1"
 }
 
 base {
@@ -179,6 +179,8 @@ project.extensions.configure<ModPublishExtension>("publishMods") {
 	curseforge {
 		type = ReleaseType.STABLE
 		projectId = project.property("publishing.curseforge").toString()
+		client = true
+		server = true
 		accessToken = providers.environmentVariable("CURSEFORGE_TOKEN")
 		minecraftVersions.add(project.property("minecraft_version") as String)
 
